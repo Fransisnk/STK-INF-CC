c44ddf817c9e22470d11ceb744572dd0c995a2ae
fransisnk <fmkolsto@student.matnat.uio.no>
2017-03-13 16:56:22 +0100

    add basic csv to pymongo function

--- database.py
+++ database.py
@@ -7,12 +7,32 @@ import sys, getopt, pprint
 class Database():
     def __init__(self):
         self.data = "res/KS_Mobile_Calls.csv"
-        self.df = pd.read_csv(self.data, delimiter=";", parse_dates=[0])
+        #self.df = pd.read_csv(self.data, delimiter=";", parse_dates=[0])
 
 
-        client = MongoClient()
-        db = client.callData
-        db.insert()
+        self.client = MongoClient()
+        self.db = self.client.db
+        self.calldb = self.db.callData
+        self.ytdb = self.db.YTData
 
+    def clearDB(self, db):
+        db.remove()
 
-    def printColumn(self, colName):
+    def csvToDB(self, csvPath, db):
+        """
+        Adds data from csv-file to mongodb. Param could be pandas-df. Index by date?
+        :param csvPath: str path to csv-file
+        :param db: pymongo collection to add data
+        :return:
+        """
+        df = pd.read_csv(csvPath, delimiter=";")
+        jsonData = json.loads(df.to_json(orient="records"))
+
+        db.insert_many(jsonData)
+        print(self.calldb.find_one())
+
+if __name__ == "__main__":
+    c = Database()
+    c.csvToDB("res/test.csv", c.calldb)
+    #c.clearDB(c.ytdb)
+    #c.clearDB(c.calldb)
