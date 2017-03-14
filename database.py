7dcf62e65e59e0db74fe2065ed50b4fb61a765ca
alteroppa <fschweinfest@hotmail.com>
2017-03-14 20:46:55 +0100

    doesn't work anymore

--- database.py
+++ database.py
@@ -24,21 +24,40 @@ class Database():
         """
         df = pd.read_csv(csvPath, delimiter=";", parse_dates=[['Call_Date', 'Time']], nrows=200)
         for date in df['Call_Date_Time']:
+            print(date)
             df = df.assign(month=self.addMonth(date))
+            #print(date.minute)
 
         jsonData = json.loads(df.to_json(orient="records"))
 
         collection.insert_many(jsonData)
 
-    def addMonth(self, datetime):
+    def addMonth(self, dt):
+        """
+        dt is type datetime.
+        takes datetime object, reads month from it, returns list[12] with all '0', except one '1' for the number of the corresponding month
+        :param dt:datetime
+        :return months:list
+        """
+        print(dt.month - 1)
         months = [0] * 12
-        months[datetime.month -1] = 1
+        months[dt.month -1] = 1
+        print('months-list:',months)
         return(months)
 
     def addQuarterlyHour(self, datetime):
-        quarters = [0] * 96
+        quarters0 = [0] * 24
+        quarters1 = [0] * 24
+        quarters2 = [0] * 24
+        quarters3 = [0] * 24
 
+        if datetime.minute == 0:
+            pass
 
+        # numberToAdd = datetime.hour * 4
+        # numberToAdd += (datetime.minute % 4)
+        # quarters[numberToAdd] = 1
+        # return(quarters)
 
 
     def addQuarterlyHour(selfs, lineOfDataframe):
