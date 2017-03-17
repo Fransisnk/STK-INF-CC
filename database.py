ba39bf5f0ff72e6d5eedaca4671f9856b92e38ce
alteroppa <fschweinfest@hotmail.com>
2017-03-17 17:00:48 +0100

    added dummydata of quarterly hours and months to database

--- database.py
+++ database.py
@@ -2,6 +2,7 @@ from pymongo import MongoClient
 import csv
 import json
 import pandas as pd
+from datetime import datetime
 import sys, getopt, pprint
 
 class Database():
@@ -26,21 +27,29 @@ class Database():
         :param db: pymongo collection to add data
         :return:
         """
-        df = pd.read_csv(csvPath, delimiter=";", index_col=[0, 1, 4], parse_dates=['Call_Date', "Time"], nrows=10)
+        df = pd.read_csv(csvPath, delimiter=";", index_col=[0, 1, 4], parse_dates=['Call_Date'], nrows=100)
         df.drop('Program', axis=1, inplace=True)
+        print(df)
 
-        #dates = []
-        #for date in df['Call_Date_Time']:
-        #    dates.append(self.addMonth(date))
-            #print(date.minute)
-        #df = df.assign(month=dates)
+        #print(df['Offered_Calls'].index.get_level_values(2))
+
+
+
+        dates = []
+        times = []
+        for date in df.index.get_level_values(0):
+            dates.append(self.addMonth(date))
+        for time in df.index.get_level_values(1):
+            times.append(self.addQuarterlyHour(time))
+        df = df.assign(month=dates)
+        df = df.assign(quarterlyHour=times)
+        print(df)
 
         jsonData = json.loads(df.reset_index().to_json(orient="records"))
         collection.insert_many(jsonData)
 
     def addMonth(self, dt):
         """
-        dt is type datetime.
         takes datetime object, reads month from it, returns list[12] with all '0', except one '1' for the number of the corresponding month
         :param dt:datetime
         :return months:list
@@ -49,14 +58,32 @@ class Database():
         months[dt.month -1] = 1
         return(months)
 
-    def addQuarterlyHour(self, datetime):
+    def addQuarterlyHour(self, dt):
+        '''
+        Transforms string to datetime object, reads hour and minute from it, and creates a 96 bit long list with
+            the first 24 bits being the 24 hours with minute == 00, the next 24 bits being the 24 hrs with minute == 15,
+            the next 24 bits being the 24 hours with minute == 30 and the last analogue but with minute == 45.
+        :param dt: string
+        :return: list
+        '''
+        newDt = datetime.strptime(dt, "%H:%M:%S") #transforms string to datetime object
         quarters0 = [0] * 24
         quarters1 = [0] * 24
         quarters2 = [0] * 24
         quarters3 = [0] * 24
+        hour = newDt.hour
+        minute = newDt.minute
 
-        if datetime.minute == 0:
-            pass
+        if minute == 0:
+            quarters0[hour] = 1
+        elif minute == 15:
+            quarters1[hour] = 1
+        elif minute == 30:
+            quarters2[hour] = 1
+        elif minute == 45:
+            quarters3[hour] = 1
+        collectedList = quarters0 + quarters1 + quarters2 + quarters3
+        return(collectedList)
 
         # numberToAdd = datetime.hour * 4
         # numberToAdd += (datetime.minute % 4)
@@ -64,8 +91,6 @@ class Database():
         # return(quarters)
 
 
-    def addQuarterlyHour(selfs, lineOfDataframe):
-        return 'a'
 
 
 
@@ -73,8 +98,9 @@ if __name__ == "__main__":
     c = Database()
     c.calldb.remove()
     c.csvToDB("res/KS_Mobile_Calls.csv", c.calldb)
-    #cursor = c.calldb.find({'month' : 1})
-    #for line in cursor:
-    #    print(line['month'])
+
+
+    cursor = c.calldb.find({'month' : 1})
+
     #c.clearDB(c.ytdb)
     c.clearDB(c.calldb)
