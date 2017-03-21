2cdeaae78ce20390a1dcd6c5321eb829028aa863
alteroppa <fschweinfest@hotmail.com>
2017-03-21 19:33:53 +0100

    updated model

--- model.py
+++ model.py
@@ -7,19 +7,43 @@ class Model(Database):
         Database.__init__(self)
 
 
-    def printColumn(self, colname, limit=0):
-        cursor = self.callCollection.find(limit)
-        result = []
-        for line in cursor:
-            print(line)
-            result.append(line[colname])
-        return result
+    def returnColumn(self, columnName, limit=0):
+        '''
+        returns the columnName of a given [columnName], limited to [limit] lines
+        :param columnName: string
+        :param limit: int
+        :return: list
+        '''
+        resultList = []
+        for line in self.callCollection.find():
+            resultList.append(line[columnName])
+        return(resultList)
+
+    def returnAllColumnNames(self):
+        keyList = []
+        for key in self.callCollection.find_one().keys():
+            keyList.append(key)
+        return(keyList)
+
+
+    def returnCombinedColumn(self, colList):
+        dataFrame = model.callCollection.find()
+        resultList = []
+        for line in dataFrame:
+            bufferList = []
+            for colName in colList:
+                bufferList += line[colName]
+            resultList.append(bufferList)
+        return(resultList)
+
 
 
 if __name__ == "__main__":
     model = Model()
 
-    #print(model.printColumn('Call_Date_Time', limit=10))
-    print(model.callCollection.find_one())
+    columnList = ['quarterlyHour', 'month']
+
+    print(model.returnCombinedColumn(columnList))
+
 
 
