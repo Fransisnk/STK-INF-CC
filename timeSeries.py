262da90b76ec348ff9a0cdc43db49415580ae176
Harjeet <harjeet.harpal@gmail.com>
2017-05-28 18:29:59 +0200

    Prediction for Fransis included

--- timeSeries.py
+++ timeSeries.py
@@ -8,8 +8,10 @@ import statsmodels.graphics.tsaplots as ts_plots
 from statsmodels.tsa.arima_model import ARIMA
 from sklearn.metrics import mean_squared_error
 from statsmodels.tsa.statespace import sarimax as sx
+from datetime import timedelta
 import warnings
 from dataframes import CallCenter
+
 
 def getTSeries(callType, bin = "1H", startDay = '8:00', endDay = '18:00'):
     """
@@ -27,12 +29,15 @@ def getTSeries(callType, bin = "1H", startDay = '8:00', endDay = '18:00'):
     data = c.binnedType(c.cdf, callType, bin, startDay, endDay)
     return pd.Series(data['Offered_Calls'], index = data.index)
 
-bestillingSeries = getTSeries('Mobile Bestilling', "1D", "00:00", "23:59")
-logBestilling = np.log(bestillingSeries)
+# # --- Let's create some series ---
+bestillingDailySeries = getTSeries('Mobile Bestilling', "1D", "00:00", "23:59")
+logBestilling = np.log(bestillingDailySeries)
+bestillingHourlySeries = getTSeries('Mobile Bestilling')
 
-# print(type(bestillingSeries))
-# print(bestillingSeries['2015'].head())
-# bestillingSeries['2015'].plot()
+# # --- Just exploring the series ---
+# print(type(bestillingDailySeries))
+# print(bestillingDailySeries['2015'].head())
+# bestillingDailySeries['2015'].plot()
 # pyplot.show()
 
 
@@ -42,25 +47,25 @@ logBestilling = np.log(bestillingSeries)
 # Y-axis = number of lags
 
 # # Autocorrelation with pandas
-# autocorrelation_plot(bestillingSeries)
+# autocorrelation_plot(bestillingDailySeries)
 # pyplot.show()
 
 # Autocorrelation and autocovariance with statsmodels
 all_plots, axes = pyplot.subplots(1,2)
-all_plots = ts_plots.plot_acf(bestillingSeries, lags=40, ax=axes[0])
-all_plots = ts_plots.plot_pacf(bestillingSeries, lags=40, ax=axes[1])
+all_plots = ts_plots.plot_acf(bestillingDailySeries, lags=40, ax=axes[0])
+all_plots = ts_plots.plot_pacf(bestillingDailySeries, lags=40, ax=axes[1])
 pyplot.suptitle('Analysis of correlations')
 pyplot.show()
 
 # # --- First difference ---
-# X = bestillingSeries.values
-# dSeries = bestillingSeries.shift(365)
-# pyplot.plot(dSeries)
-# pyplot.suptitle('Yearly differentiated bestillingSeries')
+# X = bestillingDailySeries.values
+# dSeries = bestillingDailySeries.shift(365)
+# pyplot.plot(ddSeries)
+# pyplot.suptitle('Yearly differentiated series of daily Bestilling calls')
 # pyplot.show()
 
-# # --- Removing seasonality ---
-# resample = bestillingSeries.resample('W')
+# # --- Observing the weekly mean ---
+# resample = bestillingDailySeries.resample('W')
 # weekly_mean = resample.mean()
 # print("Weekly mean: ", weekly_mean.head())
 # weekly_mean.plot()
@@ -73,7 +78,7 @@ pyplot.show()
 # pyplot.suptitle('Analysis of correlations for weekly means')
 # pyplot.show()
 #
-#
+# # --- Observing the monthly mean ---
 # resample = bestillingSeries.resample('M')
 # monthly_mean = resample.mean()
 # print("Monthly mean: ", monthly_mean.head())
@@ -87,13 +92,16 @@ pyplot.show()
 # pyplot.suptitle('Analysis of correlations for monthly mean')
 # pyplot.show()
 
-
+# # - - - M O D E L I N G - - -
+#
 # # --- ARIMA ---
-# # Fit model BESTILLING
-# model = ARIMA(bestillingSeries, order=(1,1,0))
+#
+# # -- Fit model ARIMA(1,1,0) for daily BESTILLING --
+# model = ARIMA(bestillingDailySeries, order=(1,1,0))
 # model_fit = model.fit(disp=0)
 # print(model_fit.summary())
-# # Plot residual errors
+#
+# # -- Plot residual errors --
 # residuals = DataFrame(model_fit.resid)
 # residuals.plot()
 # pyplot.suptitle('Residuals for ARIMA(1,1,0)')
@@ -103,132 +111,180 @@ pyplot.show()
 # pyplot.show()
 # print(residuals.describe())
 
-# # We decide to differentiate per weekly seasonality
+# # -- We decide to differentiate per weekly seasonality --
 # w_stationary = dSeries.shift(7).dropna(inplace=True)
 # print("Type of w_stationary: ", type(w_stationary), w_stationary[:10])
 
 
-# NEW APPROACH
+# # NEW APPROACH:
+# --- GRID SEARCH ---
 
-# # --- GRID SEARCH ---
-# # evaluate an ARIMA model for a given order (p,d,q)
-# def evaluate_sarimax_model(X, sarimax_order, best_order=None):
-#     # TODO: make sure that best order is either None (non existant, not used) or a list of 3 elements (p,d,q)
-#     # prepare training dataset
-#     train_size = int(len(X) * 0.66)
-#     train, test = X[0:train_size], X[train_size:]
-#     history = [x for x in train]
-#     # make predictions
-#     predictions = list()
-#     for t in range(len(test)):
-#         #print("sarimax_order: ", sarimax_order)
-#         #print("sarimax_order[0:3]: ", sarimax_order[0:3])
-#         if len(sarimax_order)==3:
-#             model = sx.SARIMAX(history, order=sarimax_order)
-#         elif len(sarimax_order)==4:
-#             model = sx.SARIMAX(history, order=best_order, seasonal_order=sarimax_order)
-#         else:
-#             print("Wrong number of parameters!")
-#             break
-#         model_fit = model.fit(disp=0)
-#         yhat = model_fit.forecast()[0]
-#         predictions.append(yhat)
-#         history.append(test[t])
-#     # calculate out of sample error
-#     error = mean_squared_error(test, predictions)
-#     return error
+def predict_per_point(X, sarimax_order, split_date, best_order=None):
+    """
+    Fits an ARIMA for a given (p,d,q) or
+    SARIMAX model for a given (P,D,Q,s), given also a fixed best (p,d,q)
+    depending on the number of parameters given (3 or 4) by
+    1) predicting observation by observation after fitting on training test
+    2) comparing prediction with observed value in test set
+    3) add last observed value to training set for nex prediction
+    :param X: Array of data
+    :param sarimax_order: Array of parameters
+    :param best_order: Array of best ARIMA(p,d,q)
+    :param split_date: date where we stop training and start testing
+    :return: Prediction values for test set and Mean Squared Error (and test set)
+    """
+    # TODO: make sure that best order is either None (non existent, not used) or a list of 3 elements (p,d,q)
+    # prepare training dataset
+    train, test = X[0:split_date], X[split_date:]
+    history = [x for x in train]
+    # make predictions
+    predictions = list()
+    for t in range(len(test)):
+        if len(sarimax_order)==3:
+            model = sx.SARIMAX(history, order=sarimax_order)
+        elif len(sarimax_order)==4:
+            model = sx.SARIMAX(history, order=best_order, seasonal_order=sarimax_order)
+        else:
+            print("Wrong number of parameters!")
+            break
+        model_fit = model.fit(disp=0)
+        yhat = model_fit.forecast()[0]
+        predictions.append(yhat)
+        history.append(test[t])
+    # calculate out of sample error
+    error = mean_squared_error(test, predictions)
+    return predictions, error, test
+
+
+def evaluate_pdq(dataset, p_values, d_values, q_values):
+    """
+    Evaluates combinations of p, d and q values for an ARIMA model (printing intermediate results)
+    :param dataset: Dataset or Series of data
+    :param p_values: Array of p values
+    :param d_values: Array of d values
+    :param q_values: Array of q values
+    :return: Best hyperparameters configuration, and minimum MSE of this model
+    """
+    dataset = dataset.astype('float32')
+    best_score, best_cfg = float("inf"), None
+    threefourths = int(len(dataset) * 0.75)
+    newDate = dataset.index.max() - timedelta(days=threefourths)
+    dataset.index(timedelta())
+    for p in p_values:
+        for d in d_values:
+            for q in q_values:
+                arima_order = (p,d,q)
+                print("order: ", arima_order)
+                try:
+                    mse = predict_per_point(dataset, arima_order, newDate)[1]
+                    if mse < best_score:
+                        print("We got a new best score!")
+                        best_score, best_cfg = mse, arima_order
+                    print('ARIMA(%s): MSE=%.3f' % (arima_order, mse))
+                except:
+                    continue
+    print('Best model: ARIMA(%s): MSE=%.3f' % (best_cfg, best_score))
+    return best_cfg, best_score
+
+
+def evaluate_PDQs(dataset, P_values, D_values, Q_values, s_values, best_order=(0,0,0)):
+    """
+    Evaluates combinations of P, D, Q and s values for a SARIMAX model, given the best ARIMA(p,d,q)
+    :param dataset: Dataset or Series of data
+    :param P_values: Array of P values
+    :param D_values: Array of D values
+    :param Q_values: Array of Q values
+    :param s_values: Array of s values
+    :param best_order: Array of 3 elements (p,d,q) that are the best values [given]
+    :return: Best hyperparameters configuration, and minimum MSE of this model
+    """
+    # TODO: make sure best order is a list with 3 elements
+    dataset = dataset.astype('float32')
+    best_score, best_cfg = float("inf"), None
+    for P in P_values:
+        for D in D_values:
+            for Q in Q_values:
+                for s in s_values:
+                    seasonal_order = (P,D,Q,s)
+                    print("order: ", seasonal_order)
+                    try:
+                        mse = predict_per_point(dataset, seasonal_order, best_order)[1]
+                        if mse < best_score:
+                            print("We got a new best score!")
+                            best_score, best_cfg = mse, seasonal_order
+                        print('SARIMAX(%s)(%s): MSE=%.3f' % (best_order, seasonal_order, mse))
+                    except:
+                        continue
+    print('Best model: SARIMAX(%s)(%s): MSE=%.3f' % (best_order, best_cfg, best_score))
+    return best_cfg, best_score
+
+# # # --- EVALUATION OF PARAMETERS (to be run once!) ---
 #
-# # evaluate combinations of p, d and q values for an ARIMA model
-# def evaluate_pdq(dataset, p_values, d_values, q_values):
-#     dataset = dataset.astype('float32')
-#     best_score, best_cfg = float("inf"), None
-#     for p in p_values:
-#         for d in d_values:
-#             for q in q_values:
-#                 arima_order = (p,d,q)
-#                 print("order: ", arima_order)
-#                 try:
-#                     mse = evaluate_sarimax_model(dataset, arima_order)
-#                     if mse < best_score:
-#                         print("We got a new best score!")
-#                         best_score, best_cfg = mse, arima_order
-#                     print('ARIMA(%s): MSE=%.3f' % (arima_order, mse))
-#                 except:
-#                     continue
-#     print('Best model: ARIMA(%s): MSE=%.3f' % (best_cfg, best_score))
-#     return best_cfg, best_score
-#
-# def evaluate_PDQs(dataset, P_values, D_values, Q_values, s_values, best_order=(0,0,0)):
-#     # TODO: make sure best order is a list with 3 elements
-#     dataset = dataset.astype('float32')
-#     best_score, best_cfg = float("inf"), None
-#     for P in P_values:
-#         for D in D_values:
-#             for Q in Q_values:
-#                 for s in s_values:
-#                     seasonal_order = (P,D,Q,s)
-#                     print("order: ", seasonal_order)
-#                     try:
-#                         mse = evaluate_sarimax_model(dataset, seasonal_order, best_order)
-#                         if mse < best_score:
-#                             print("We got a new best score!")
-#                             best_score, best_cfg = mse, seasonal_order
-#                         print('SARIMAX(%s)(%s): MSE=%.3f' % (best_order, seasonal_order, mse))
-#                     except:
-#                         continue
-#     print('Best model: SARIMAX(%s)(%s): MSE=%.3f' % (best_order, best_cfg, best_score))
-#     return best_cfg, best_score
-#
-# # evaluate parameters
+# # -- For DAILY BINNING --
 # p_values = [1, 2, 3, 7]
 # d_values = range(0, 2)
 # q_values = range(0, 3)
 #
 # S = range(0, 3)
-# s_values = [0,7]
+# s_values = [0, 7]
 #
 # warnings.filterwarnings("ignore")
-# arima_res = evaluate_pdq(bestillingSeries.values, p_values, d_values, q_values)
-# sarimax_res = evaluate_PDQs(bestillingSeries.values, S, S, S, s_values, arima_res[0])
+# daily_arima_res = evaluate_pdq(bestillingDailySeries.values, p_values, d_values, q_values)
+# daily_sarimax_res = evaluate_PDQs(bestillingDailySeries.values, S, S, S, s_values, daily_arima_res[0])
+#
+# # -- For HOURLY BINNING --
+# p_values = [1, 2, 11, 77]
+# d_values = range(0, 2)
+# q_values = range(0, 3)
+#
+# S = range(0, 3)
+# s_values = [0, 11, 77]
+#
+# warnings.filterwarnings("ignore")
+# hourly_arima_res = evaluate_pdq(bestillingHourlySeries.values, p_values, d_values, q_values)
+# hourly_sarimax_res = evaluate_PDQs(bestillingHourlySeries.values, S, S, S, s_values, hourly_arima_res[0])
 
-# --- SARIMAX: how the best model looks like ---
-smodel = sx.SARIMAX(bestillingSeries, exog=None, order=(7,1,0), seasonal_order=(1,1,1,7), trend='t')
-smodel_fit = smodel.fit(disp=0)
-yhat = smodel_fit.fittedvalues
-print(smodel_fit.summary())
+
+# --- SARIMAX: how the best model looks like for daily calls on all the dataset ---
+daily_model = sx.SARIMAX(bestillingDailySeries, exog=None, order=(7,1,0), seasonal_order=(1,1,1,7), trend='t')
+daily_model_fit = daily_model.fit(disp=0)
+yhat = daily_model_fit.fittedvalues
+print(daily_model_fit.summary())
 # plot residual errors
-residuals = DataFrame(smodel_fit.resid)
+residuals = DataFrame(daily_model_fit.resid)
 residuals.plot()
-pyplot.suptitle('Residuals for SARIMAX(1,1,0,7)')
+pyplot.suptitle('Residuals for SARIMAX(7,1,0)(1,1,1,7)')
 pyplot.show()
 residuals.plot(kind='kde')
-pyplot.suptitle('Residuals for SARIMAX(1,1,0,7)')
+pyplot.suptitle('Residuals for SARIMAX(7,1,0)(1,1,1,7)')
 pyplot.show()
 print(residuals.describe())
-# This works
-
-# --- Prediction of last two weeks with SARIMAX ---
-# TODO: conform splitting of training and testing data to what we have in linearRegression.py in readAndPrepareData()
-# train, test = monthly_mean[:-2], monthly_mean[-2:]
-size = int(len(bestillingSeries) * 0.66)
-train, test = bestillingSeries[0:size], bestillingSeries[size:len(bestillingSeries)]
-history = [x for x in train]
-predictions = list()
-for t in range(len(test)):
-    model = sx.SARIMAX(bestillingSeries, exog=None, order=(7,1,0), seasonal_order=(1,1,1,7), trend='t')
-    model_fit = model.fit(disp=0)
-    output = model_fit.forecast()
-    yhat = output[0]
-    predictions.append(yhat)
-    obs = test[t]
-    history.append(obs)
-print('predicted=%f, expected=%f' % (yhat, obs))
-error = mean_squared_error(test, predictions)
-print('Test MSE: %.3f' % error)
-# plot
-pyplot.plot(test, 'k-', label='actual calls', alpha=0.7)
-pyplot.plot(predictions, color='red', label='time series prediction', linewidth=2, alpha=0.9)
+pyplot.plot(bestillingDailySeries, 'k-', label='actual calls', alpha=0.7)
+pyplot.plot(yhat, color='red', label='time series fitted model', linewidth=2, alpha=0.9)
+pyplot.legend()
 pyplot.show()
+# # This works
 
-# This doesn't work
-# TODO: figure out why it's running forever and doesn't get to showing the plots
+# # --- Prediction of last two weeks for SARIMAX(7,1,0)(1,1,1,7) with predict() ---
+maxdate = bestillingDailySeries.index.max()
+start_predict_from = maxdate - timedelta(14)
+yhat_pred = daily_model_fit.predict(end = "2017-05-30")
+# plot prediction
+pyplot.plot(bestillingDailySeries, 'k-', label='actual calls', alpha=0.7)
+pyplot.plot(yhat_pred, color='blue', label='time series prediction', linewidth=2, alpha=0.9)
+pyplot.legend()
+pyplot.show()
+# # This works
+
+# # --- Prediction of last two weeks for SARIMAX(7,1,0)(1,1,1,7)  ---
+# # TODO: conform splitting of training and testing data to what we have in linearRegression.py in readAndPrepareData()
+# predictions = predict_per_point(bestillingDailySeries, [1,1,1,7], best_order=[7,1,0], "2017-05-30")
+# print('predicted=%f, expected=%f' % (predictions[0], predictions[2]))
+# print('Test MSE: %.3f' % predictions[1])
+# # plot
+# pyplot.plot(predictions[2], 'k-', label='actual calls', alpha=0.7)
+# pyplot.plot(predictions[0], color='red', label='time series prediction', linewidth=2, alpha=0.9)
+# pyplot.show()
+# # This doesn't work
+# # TODO: figure out why it's running forever and doesn't get to showing the plots
+
